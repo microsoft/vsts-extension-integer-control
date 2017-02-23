be420677b6b63c329dec2ca22452e8f959de9837
Otto Streifel <ottost@microsoft.com>
2017-02-23 10:30:48 -0800

    Adds ctr + s handler

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,5 +1,16 @@
 import { Controller } from "./control";
 import { IWorkItemLoadedArgs, IWorkItemFieldChangedArgs } from "TFS/WorkItemTracking/ExtensionContracts";
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+
+// save on ctr + s
+$(window).bind("keydown", function (event: JQueryEventObject) {
+    if (event.ctrlKey || event.metaKey) {
+        if (String.fromCharCode(event.which) === "S") {
+            event.preventDefault();
+            WorkItemFormService.getService().then((service) => service.beginSaveWorkItem($.noop, $.noop));
+        }
+    }
+});
 
 var control: Controller;
 
